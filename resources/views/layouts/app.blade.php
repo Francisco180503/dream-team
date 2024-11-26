69babc2b28b655cc4f013a1f3f85c04ecbc348fd
Mary22Martel <2020210311@gmail.com>
2024-11-26 00:30:12 -0500

    instalacion de tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,13 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <script type="module" src="{{ mix('js/app.js') }}" defer></script>
+
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
 
         <!-- Styles -->
         @livewireStyles
