a51b5a6b8bba17b318d910fc1a2ec6500c55f25e
Mary22Martel <2020210311@gmail.com>
2024-11-26 10:25:06 -0500

    roles a medias xd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,4 +24,9 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'redirect.role' => \App\Http\Middleware\RedirectBasedOnRole::class,
+        // Otros middlewares
+    ];
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,4 +65,12 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'redirect.role' => \App\Http\Middleware\RedirectBasedOnRole::class,
+        // Otros middlewares
+    ];
+
+   
 }
